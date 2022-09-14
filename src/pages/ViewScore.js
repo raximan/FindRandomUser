f3da8547a272f95b67b3c02b1fcf8581256565ee
İzzetcan Soytürk <izzetcansoyturk0600@gmail.com>
2022-09-14 14:17:56 +0300

    first change

--- src/pages/ViewScore.js
+++ src/pages/ViewScore.js
@@ -3,7 +3,7 @@ export default function ViewScore(){
     let {userName} = useParams()
     
     return(<div>
-            <h1>Hey {userName} </h1>
+            <h1>Your name is: {userName} </h1>
           </div>
     )
           
