86e5ec1c6a0ccc0d28e0d7f89c7917abd7a6f471
İzzetcan Soytürk <izzetcansoyturk0600@gmail.com>
2022-09-16 15:23:19 +0300

    'unused dependency error' fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import FindRandomUserGame from './pages/FindRandomUserGame'
-import{BrowserRouter,Routes,Route,Switch} from "react-router-dom";
+import{BrowserRouter,Routes,Route} from "react-router-dom";
 import Home from './pages/Home';
 import Fail from './pages/Fail';
 import Congrats from './pages/Congrats';
