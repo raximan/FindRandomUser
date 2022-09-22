29eefbfc0570fcc7b45c3dae7fcc17a227b7ce48
İzzetcan Soytürk <izzetcansoyturk0600@gmail.com>
2022-09-22 12:40:19 +0300

    css added

--- src/pages/FindRandomUserGame.js
+++ src/pages/FindRandomUserGame.js
@@ -1,68 +1,88 @@
+import "../css/FindRandomUser.css"
 import {useState,useEffect} from "react";
 //import axios from 'axios';
 import { useNavigate } from "react-router-dom";
 import { datas as randomDatas } from "../randomUserDatas";
+import { Button,Grid,Typography } from "@mui/material";
+//Robofont
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 export default function FindRandomUserGame() {
 
     const [dataIsLoaded,setdataIsLoaded] = useState(false)
     const [user,setUser] = useState(null)
     const [levelPassed,setlevelPassed]=useState(false)
     const [currentRound,setcurrentRound]=useState(1)
-    const [buttonDatas,setbuttonDatas]=useState(randomDatas.sort(() => 0.5 -Math.random()).slice(0,4))
+    const [ButtonDatas,setButtonDatas]=useState(randomDatas.sort(() => 0.5 -Math.random()).slice(0,4))
     //const userData = window.localStorage.getItem('user1');
     useEffect(() => {
-          //setbuttonDatas(randomDatas.sort(() => 0.5 -Math.random()).slice(0,4))
-          setUser(buttonDatas[Math.floor(Math.random() * 4)]);
+          //setButtonDatas(randomDatas.sort(() => 0.5 -Math.random()).slice(0,4))
+          setUser(ButtonDatas[Math.floor(Math.random() * 4)]);
           setdataIsLoaded(true)
-          console.log(buttonDatas)
+          console.log(ButtonDatas)
           console.log(user)  
           console.log(currentRound)                                                                              
       }, [levelPassed]); // eslint-disable-line react-hooks/exhaustive-deps
 
     let navigator= useNavigate()
 
-    const handleClick =(button)=>{
-        if(button.imageUrl===user.imageUrl){
+    const handleClick =(Button)=>{
+        if(Button.imageUrl===user.imageUrl){
             if(currentRound===5){
-                setbuttonDatas(randomDatas.sort(() => 0.5 -Math.random()).slice(5,9))
+                setButtonDatas(randomDatas.sort(() => 0.5 -Math.random()).slice(5,9))
                 navigator("/congrats")
             }
             else{
-            setbuttonDatas(randomDatas.sort(() => 0.5 -Math.random()).slice(5,9))
+            setButtonDatas(randomDatas.sort(() => 0.5 -Math.random()).slice(5,9))
             setcurrentRound(num=>num+1)
             setdataIsLoaded(false)
             setlevelPassed(!levelPassed)
             }   
         }
-        else if(button.imageUrl!==user.imageUrl){
+        else if(Button.imageUrl!==user.imageUrl){
         navigator("/fail/"+(currentRound-1))
         }
        }
 
 
-    return <div style={{padding:"1% 40% "}} id="FindRandomUser" >
-            <h1>Round {dataIsLoaded ? currentRound:"null"}</h1>
-            <img src={dataIsLoaded ? user.imageUrl:"null"}  alt="Person" />
-            <div>
-                <h3> {dataIsLoaded ? buttonDatas[0].name:"null"} - { dataIsLoaded ? buttonDatas[0].country:"null"}  </h3>
-                <button  onClick={()=>handleClick(buttonDatas[0])} disabled={dataIsLoaded ? false : true}>Choose</button> 
-            </div>
-            <div>
-                <h3> {dataIsLoaded ? buttonDatas[1].name:"null"} - { dataIsLoaded ? buttonDatas[1].country:"null"}  </h3>
-                <button  onClick={()=>handleClick(buttonDatas[1])} disabled={dataIsLoaded ? false : true}>Choose</button> 
-            </div>
-            <div>
-                <h3> {dataIsLoaded ? buttonDatas[2].name:"null"} - { dataIsLoaded ? buttonDatas[2].country:"null"}  </h3>
-                <button  onClick={()=>handleClick(buttonDatas[2])} disabled={dataIsLoaded ? false : true}>Choose</button> 
-            </div>
-            <div>
-                <h3> {dataIsLoaded ? buttonDatas[3].name:"null"} - { dataIsLoaded ? buttonDatas[3].country:"null"}  </h3>
-                <button  onClick={()=>handleClick(buttonDatas[3])} disabled={dataIsLoaded ? false : true}>Choose</button> 
-            </div>
-          <div  style={{ padding:"10% 0% 70%"}}>
-          <button style={{margin:"30px", padding:"1% 4% "}} onClick={()=>navigator("/")}>Return Home</button>
-          </div>
-          <h6 style={{padding:"1% 10% "}}>{dataIsLoaded ? user.name:"null"}</h6>
+    return <div  id="FindRandomUser" >
+
+            <Typography variant="h5" className="centerinFindRandomUser" id="Round">ROUND: {dataIsLoaded ? currentRound:"null"}</Typography>
+            <img className="centerinFindRandomUser" src={dataIsLoaded ? user.imageUrl:"null"}  alt="Person" />
+            <Grid container spacing={2}>
+            <Grid className="GridItem" item xs={6}>
+                
+                    <Typography className="GridItemText"  > {dataIsLoaded ? ButtonDatas[0].name:"null"} - { dataIsLoaded ? ButtonDatas[0].country:"null"}  </Typography>
+                    <Button className="GridButton" variant="contained"  onClick={()=>handleClick(ButtonDatas[0])} disabled={dataIsLoaded ? false : true}>Choose</Button> 
+                   
+            </Grid>
+            <Grid className="GridItem" item xs={6}>
+                
+                    <Typography className="GridItemText" > {dataIsLoaded ? ButtonDatas[1].name:"null"} - { dataIsLoaded ? ButtonDatas[1].country:"null"}  </Typography>
+                    <Button className="GridButton" variant="contained"  onClick={()=>handleClick(ButtonDatas[1])} disabled={dataIsLoaded ? false : true}>Choose</Button> 
+                
+            </Grid>
+            <Grid className="GridItem" item xs={6}>
+                
+                    <Typography className="GridItemText" > {dataIsLoaded ? ButtonDatas[2].name:"null"} - { dataIsLoaded ? ButtonDatas[2].country:"null"}  </Typography>
+                    <Button className="GridButton" variant="contained"  onClick={()=>handleClick(ButtonDatas[2])} disabled={dataIsLoaded ? false : true}>Choose</Button> 
+                
+            </Grid>
+            <Grid className="GridItem" item xs={6}>
+                
+                    <Typography className="GridItemText" > {dataIsLoaded ? ButtonDatas[3].name:"null"} - { dataIsLoaded ? ButtonDatas[3].country:"null"}  </Typography>
+                    <Button className="GridButton" variant="contained"  onClick={()=>handleClick(ButtonDatas[3])} disabled={dataIsLoaded ? false : true}>Choose</Button> 
+                
+            </Grid>
+            </Grid>
+    
+
+          <Button className="centerinFindRandomUser" id="ReturnHome"  onClick={()=>navigator("/")}>Return Home</Button>
+
+          <h6 style={{padding:"45% 10% "}}>{dataIsLoaded ? user.name:"null"}</h6>
          </div>
   
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,27 @@
+import "../css/Home.css"
 import {useNavigate} from "react-router-dom"
+import { Button,Typography } from "@mui/material"
+// import {AnimatedBg} from  "react-animated-bg";
 export default function Home(){
-    let navigate =useNavigate()
+   let navigate =useNavigate()
 
 
-return<div>
-            <h1 style={{padding:"4% 34% "}}>Welcome to Find Random User Game</h1>
-            <h2 style={{padding:"1% 34% "}}>All you need to do is guess the people by their image </h2>
-            <h3 style={{padding:"1% 37% "}}> Look at the bottom for the answer if you feel stuck</h3>
-            <div style={{padding:"10% 45% "}} >
-            <button  onClick={()=>navigate("/game")}>Press to Start</button>
-            </div>
-    </div>
+return(<div className="Home">
+{/* 
+            <AnimatedBg colors={imageUrls}
+            duration={2}
+            delay={1}
+            timingFunction="ease-out"
+            className="animated-section animated-images">
+              <button  onClick={()=>navigate("/game")}>Press to Start</button>
+            </AnimatedBg> */}
+
+
+            <Typography className="h1" variant="h4">Welcome to Find Random User Game</Typography>
+            <Typography className="h2" variant="h5">All you need to do is guess the people by their image </Typography>
+            <Typography className="h3" variant="h6"> Look at the bottom for the answer if you feel stuck</Typography>
+          
+            <Button className="Buttonhey" onClick={()=>navigate("/game")}>Press to Start</Button>
+            
+    </div>)
 }
