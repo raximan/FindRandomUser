e5153056f15e89184432ba2ea4d4dbf9a20e0307
İzzetcan Soytürk <izzetcansoyturk0600@gmail.com>
2022-09-30 22:31:29 +0300

    footer.js refactored

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -2,8 +2,8 @@ import "../css/Footer.css"
 export default function Footer(){
     return (<footer class="Footer">
 
-  <p style={{padding:"3% 44% 1%"}} >© Copyright 2022</p>
-  <p style={{padding:"0% 42% 0%"}} >Powered by İzzetcan Soytürk</p>
+  <p  >© Copyright 2022</p>
+  <p  >Powered by İzzetcan Soytürk</p>
 
 </footer>
 )
